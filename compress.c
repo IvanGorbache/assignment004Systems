ac90bef48797db7a49b5962d78afa0df147cf697
ivanGorbache <ivan.gorbache@msmail.ariel.ac.il>
2024-03-02 17:00:33 +0200

    Fixed compression algorithm to work on 4bits

--- compress.c
+++ compress.c
@@ -1,55 +1,87 @@
 #include "compress.h"
 
+struct _fourBitNum
+{
+    unsigned int _data:  4;
+}typedef fourBitNum;
+
 void compress(const char* filePath)
 {
     FILE *fptr, *binfptr;
-    char *code = malloc(1000 * sizeof(char)), 
-    *binaryOutFile = malloc(((strlen(filePath)+1) * sizeof(char)));
-
-    fptr = fopen(filePath, "r");
-    int ch, index = 0;
-    while ((ch = fgetc(fptr)) != EOF && index < 1000) {
-        // Perform bitwise NOT operation on the character
-        code[index++] = ~(char)ch;
-    }
-    code[index] = '\0';
-    fclose(fptr);
+    char *binaryOutFile = malloc(((strlen(filePath)+1) * sizeof(char))), ch; 
+    unsigned char compressed = 0;
+    int cursor = 4;
+    fourBitNum code;
 
     strcpy(binaryOutFile,filePath);
     binaryOutFile[strlen(binaryOutFile)-3] = 'b';
     binaryOutFile[strlen(binaryOutFile)-2] = 'i';
     binaryOutFile[strlen(binaryOutFile)-1] = 'n';
 
+    fptr = fopen(filePath, "r");
     binfptr = fopen(binaryOutFile,"wb");
-    fwrite(code, sizeof(char), strlen(code), binfptr);
+    
 
+    while((ch=fgetc(fptr))!=EOF && ch != '\n' && ch != '\r')
+    {
+        if (ch >= '0' && ch <= '9') {
+            code._data = ~(ch - '0');
+            compressed |= (code._data<<cursor);
+            cursor-=4;
+            
+            if(cursor<0)
+            {
+                fwrite(&compressed, sizeof(unsigned char), 1, binfptr);
+                compressed = 0;
+                cursor = 4;
+            }
+        }
+    }
+    
+    if(cursor == 0)
+    {
+        fwrite(&compressed, sizeof(unsigned char), 1, binfptr);
+    }
+    
+
+
+    fclose(fptr);
     fclose(binfptr);
-    free(code);
     free(binaryOutFile);
 }
 
 void decompress(const char* filePath)
 {
     FILE *fptr, *binfptr;
-    char *textOutFile = malloc((strlen(filePath+1) * sizeof(char)));
-    unsigned char *buffer = malloc(1000 * sizeof(unsigned char));
-
-    binfptr = fopen(filePath, "rb");
-    fread(buffer,sizeof(buffer),1,binfptr);
-
-    fclose(binfptr);
-
-    strcpy(textOutFile,filePath);
+    char *textOutFile = malloc((strlen(filePath) + 1) * sizeof(char));
+    fourBitNum code;
+    unsigned char compressed = 0;
+    int cursor = 0;
+    
+    strcpy(textOutFile, filePath);
     textOutFile[strlen(textOutFile)-3] = 't';
     textOutFile[strlen(textOutFile)-2] = 'x';
     textOutFile[strlen(textOutFile)-1] = 't';
 
-    fptr = fopen(textOutFile,"w");
-    for(int i = 0;i<strlen(buffer);i++)
-    {
-        fputc(~buffer[i], fptr);
+    fptr = fopen(textOutFile, "w");
+    binfptr = fopen(filePath, "rb");
+
+    while (fread(&compressed, sizeof(unsigned char), 1, binfptr) == 1) {
+        compressed = ~compressed;
+        for(int i = 0;i<2;i++)
+        {
+            code._data = (compressed >> 4) & 0x0F;
+            compressed <<= 4;
+            
+            if(code._data!=15)
+            {
+                fprintf(fptr,"%c",code._data+'0');
+            }
+        }
+
     }
+
     fclose(fptr);
-    free(buffer);
+    fclose(binfptr);
     free(textOutFile);
 }
--- compress.h
+++ compress.h
@@ -2,6 +2,10 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define SIZE 1000
+
 void compress(const char* filePath);
 
 void decompress(const char* filePath);
+
+ 
